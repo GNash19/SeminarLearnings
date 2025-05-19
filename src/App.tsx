9d081febdf8efe21f982fffe8149407466ccc596
GNash19 <ngolosino_230000002598@uic.edu.ph>
2025-05-19 13:09:14 +0800

    Updated Home page and added new Seminar content

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-// App.tsx
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
-import Seminar1 from "./components/seminars/1"; 
-import Documentation from "./components/Documentation"; 
+import Seminar1 from "./components/seminars/1";
+import Documentation from "./components/Documentation";
 
 const App: React.FC = () => {
   return (
-    <Router>
+    <Router basename="/SeminarLearnings">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/seminars/1" element={<Seminar1 />} />
         <Route path="/documentation" element={<Documentation />} />
-        {/* Add other routes as needed */}
+        {/* Add 404 fallback */}
+        <Route path="*" element={<Home />} />
       </Routes>
     </Router>
   );
